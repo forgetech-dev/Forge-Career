517c3140a3ef8fb101c8552b26deac3b7f1e8b28
Yucheng Huang <yuh032@ucsd.edu>
2022-07-03 03:08:16 -0700

    Add: added bottomNaviMenu and fragment

--- app/src/main/java/com/example/forgecareer/MainActivity.java
+++ app/src/main/java/com/example/forgecareer/MainActivity.java
@@ -1,14 +1,44 @@
 package com.example.forgecareer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        replaceFragment(new HomeFragment());
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.page_1:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.page_2:
+                    replaceFragment(new CompanyFragment());
+                    break;
+                case R.id.page_3:
+                    replaceFragment(new InterviewFragment());
+                    break;
+            }
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
