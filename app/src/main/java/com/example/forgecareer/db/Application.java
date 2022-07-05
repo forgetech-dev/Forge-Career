6151e7b8df8442ea9c1ba11d52964ffb68f16d02
Yucheng Huang <yuh032@ucsd.edu>
2022-07-04 17:09:00 -0700

    Add: added fields for Application

--- app/src/main/java/com/example/forgecareer/db/Application.java
+++ app/src/main/java/com/example/forgecareer/db/Application.java
@@ -1,10 +1,40 @@
 package com.example.forgecareer.db;
 
+import java.util.Date;
+
 public class Application {
+
+    /**
+     * Sample Application data layout
+     *  CompanyName: Amazon (mandatory)
+     *  JobType: Full-Time (mandatory)
+     *  PositionType: SWE (mandatory)
+     *  StartDate: Summer23 (mandatory)
+     *  AppliedDate: 2022-09-01 (mandatory)
+     *  Priority: High (mandatory)
+     *  Interview: False (mandatory)
+     *  InterviewDate: Null (optional)
+     *  InterviewRound: 0 (Optional)
+     */
     String CompanyName;
     String JobType;
+    String PositionType;
+    String StartDate;
+    String Status;
+    Date ApplicationDate;
+    String Priority;
+    Boolean Interview;
+    Date InterviewDate;
 
+
+    /**
+     * Empty constructor
+     */
     public Application() {}
+
+    /**
+     * Full Constructor
+     */
     public Application(String companyName, String jobType) {
         CompanyName = companyName;
         JobType = jobType;
---
+++ app/src/main/java/com/example/forgecareer/utils/Constants.java
@@ -0,0 +1,28 @@
+package com.example.forgecareer.utils;
+
+/**
+ * Created by Yucheng Huang on 7/4/2022
+ */
+public class Constants {
+    public static final String[] JOBTYPE = {
+            "Full Time",
+            "Intern"
+    };
+
+    public static final String[] STARTDATE = {
+            "FALL 22",
+            "Winter 23",
+            "Spring 23",
+            "Summer 23",
+            "Fall 23",
+            "Later than Fall 23"
+    };
+
+    public static final String[] PRIORITY = {
+            "Ultra",
+            "High",
+            "Medium",
+            "Low"
+    };
+
+}
